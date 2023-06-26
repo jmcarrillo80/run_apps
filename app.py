512902c8b45ced81336598e70658ee46002529cc
Jorge.Carrillo1 <Jorge.Carrillo1@kiewit.com>
2023-06-26 15:41:59 -0500

    tables parameters fixed

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from modulos.subprocessRunTask import RunTask
 from modulos.subprocessParameters import ProjectParameters
-import json
 from pathlib import Path
+from datetime import datetime
+from pytz import timezone
+import json
 import os
 
 
@@ -18,11 +20,10 @@ def main():
     parameters = ProjectParameters(project_dict=project_dict, report_adapter_config_dict=s3d_shared_config_dict)
     project_s3d_parameters_dict = parameters.getParameters()    
 
-    results = []
-    for project_id, project_params in project_s3d_parameters_dict.items():        
+    for project_id, project_params in project_s3d_parameters_dict.items():
         root_project = ROOT_DIRECTORY/project_id
         if not os.path.exists(root_project):
-            os.makedirs(root_project)        
+            os.makedirs(root_project)
         subprocess_directories = ProjectParameters.createDirectory({'root':root_project, 'subdirectories':['ini_s3d', 'output_s3d', 'logFilesExtractions_s3d', 'logFilesConversions_s3d', 'parquetFiles_s3d']})
         for asset_type, asset_type_config in project_params.items():
             conversion_params = {}
@@ -30,45 +31,38 @@ def main():
             config_file = asset_type_config['s3d_adapter_ini_file']
             filter = asset_type_config['filter']
             database_file = asset_type_config['output_database']
-            stdout_file = subprocess_directories['logFilesExtractions_s3d']/f"{asset_type}_stdout.txt"
-            stderr_file = subprocess_directories['logFilesExtractions_s3d']/f"{asset_type}_stderr.txt"
+            now_date = datetime.now(timezone('America/Chicago')).strftime('%m-%d-%Y')
+            stdout_file = subprocess_directories['logFilesExtractions_s3d']/f"{asset_type}_stdout_{now_date}.txt"
+            stderr_file = subprocess_directories['logFilesExtractions_s3d']/f"{asset_type}_stderr_{now_date}.txt"
             conversion_params['directory_db'] = f'"{subprocess_directories["output_s3d"]}"'
             conversion_params['db_name'] = database_file.split('.')[0]
             conversion_params['db_fileformat'] = database_file.split('.')[1]
             conversion_params['tables'] = asset_type_config['table_name']
             conversion_params['directory_parquet'] = f'"{subprocess_directories["parquetFiles_s3d"]}"'
-            conversion_params['stdout_file'] = subprocess_directories['logFilesConversions_s3d']/f"{database_file.split('.')[0]}_stdout.txt"
-            conversion_params['stderr_file'] = subprocess_directories['logFilesConversions_s3d']/f"{database_file.split('.')[0]}_stderr.txt"
+            conversion_params['stdout_file'] = subprocess_directories['logFilesConversions_s3d']/f"{database_file.split('.')[0]}_stdout_{now_date}.txt"
+            conversion_params['stderr_file'] = subprocess_directories['logFilesConversions_s3d']/f"{database_file.split('.')[0]}_stderr_{now_date}.txt"
+            extraction_run_args = f'"C:\Program Files (x86)\CAXperts\\3D ReportAdapter\\3D ReportAdapter.exe" -plant:{plant} -config:{subprocess_directories["ini_s3d"]}\{config_file}\n \
+                                    -filter:{filter}\n \
+                                    -output:{subprocess_directories["output_s3d"]}\{database_file} -cleanrules\n \
+                                    -forceexit:1'
 
-            subprocess_run_args = f'"C:\Program Files (x86)\CAXperts\\3D ReportAdapter\\3D ReportAdapter.exe" -plant:{plant} -config:{subprocess_directories["ini_s3d"]}\{config_file}\n \
-            -filter:{filter}\n \
-            -output:{subprocess_directories["output_s3d"]}\{database_file} -cleanrules\n \
-            -forceexit:1'   
-            
-            taskObj = RunTask(subprocess_run_args=subprocess_run_args, cwd=Path(r'C:\Program Files (x86)\CAXperts\\3D ReportAdapter'), stdout_file=stdout_file, stderr_file=stderr_file)
+            taskObj = RunTask(subprocess_run_args=extraction_run_args, cwd=Path(r'C:\Program Files (x86)\CAXperts\\3D ReportAdapter'), stdout_file=stdout_file, stderr_file=stderr_file)
+            print('Subprocess running ...')
+            print(f'plant: {plant}\ndirectory_ini: {subprocess_directories["ini_s3d"]}\nini_file: {config_file}\nfilter: {filter}\ndirectory_database: {subprocess_directories["output_s3d"]}\ndatabase_file: {database_file}')            
             result =  taskObj.subprocess_run()
-            results.append((taskObj, result, conversion_params))
-            print(f'subprocess ID: {result.pid}\nplant: {plant}\ndirectory_ini: {subprocess_directories["ini_s3d"]}\nini_file: {config_file}\nfilter: {filter}\ndirectory_database: {subprocess_directories["output_s3d"]}\ndatabase_file: {database_file}\n')
-
-
-    for taskObj, result, conversion_params in results:
-        returnCode = taskObj.subprocess_logfiles(result)
-        if returnCode == 0:
-            print('subprocess ID: {} --> executed successfully\n'.format(result.pid))
-            subprocess_conversion_run_args = f'venv\Scripts\python app.py {conversion_params["directory_db"]} {conversion_params["db_name"]} {conversion_params["db_fileformat"]} {conversion_params["tables"]} {conversion_params["directory_parquet"]} parquet'
-            conversionTaskObj = RunTask(subprocess_run_args=subprocess_conversion_run_args, cwd=APP_DIRECTORY/'DatabaseTablesToParquet', stdout_file=conversion_params['stdout_file'], stderr_file=conversion_params['stderr_file'])
-            resultConversion = conversionTaskObj.subprocess_run()
-            print('Tables Conversion:')
-            print(f'subprocess ID: {resultConversion.pid}\nparent subprocess ID: {result.pid}\ndatabase: {conversion_params["db_name"]}\ntables: {conversion_params["tables"]}\n')
-            returnCodeConversion = conversionTaskObj.subprocess_logfiles(resultConversion)
-            if returnCodeConversion == 0:
-                print('subprocess ID: {} --> executed successfully\n'.format(resultConversion.pid))
+            if result.returncode == 0:
+                print('subprocess ID: {} --> executed successfully\n'.format(result.pid))
+                conversion_run_args = f'venv\Scripts\python app.py {conversion_params["directory_db"]} {conversion_params["db_name"]} {conversion_params["db_fileformat"]} "{conversion_params["tables"]}" {conversion_params["directory_parquet"]} parquet'
+                conversionTaskObj = RunTask(subprocess_run_args=conversion_run_args, cwd=APP_DIRECTORY/'DatabaseTablesToParquet', stdout_file=conversion_params['stdout_file'], stderr_file=conversion_params['stderr_file'])
+                print('Tables Conversion:')
+                print(f'parent subprocess ID: {result.pid}\ndatabase: {conversion_params["db_name"]}\ntables: {conversion_params["tables"]}')
+                resultConversion = conversionTaskObj.subprocess_run()
+                if resultConversion.returncode == 0:
+                    print('subprocess ID: {} --> executed successfully\n'.format(resultConversion.pid))
+                else:
+                    print('subprocess ID: {} --> NOT executed successfully\n'.format(resultConversion.pid))
             else:
-                print('subprocess ID: {} --> NOT executed successfully\n'.format(resultConversion.pid))
-        else:
-            print('subprocess ID: {} --> NOT executed successfully\n'.format(result.pid))
-
-
+                print('subprocess ID: {} --> NOT executed successfully\n'.format(result.pid))
 
 
 
--- modulos/subprocessRunTask.py
+++ modulos/subprocessRunTask.py
@@ -1,5 +1,5 @@
 import subprocess
-
+import time
 
 class RunTask():
 
@@ -12,15 +12,20 @@ class RunTask():
     
     def subprocess_run(self):
         result =  subprocess.Popen(self.subprocess_run_args, cwd=self.cwd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True)
-        print('Subprocess running ...')
-        return result
-           
-    def subprocess_logfiles(self, result:subprocess.Popen):
-        result.wait()
-        (out,err) = result.communicate()
-        with open(self.stdout_file, "w") as f_stdout:
-            f_stdout.write(out)
-        with open(self.stderr_file, "w") as f_stderr:
-            f_stderr.write(err)
-        return result.returncode    
-
+        out = ''
+        err = ''
+        while result.poll() == None:
+            for line in result.stdout:
+                print(line)
+                out += line+'\n'
+            for line in result.stderr:
+                print(line)
+                err += line+'\n'
+            time.sleep(1)
+        if out != '':
+            with open(self.stdout_file, "w") as f_stdout:
+                f_stdout.write(out)
+        if err != '':
+            with open(self.stderr_file, "w") as f_stderr:
+                f_stderr.write(err)
+        return result           
